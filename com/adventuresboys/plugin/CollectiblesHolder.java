f92bdd645b8bf11a7540d1e17375cc42d5781b4f
SpaceyDCO <55423386+SpaceyDCO@users.noreply.github.com>
2021-10-03 03:58:50 -0400

    Inventory Holder
    Clase agregada para poder ser capaz de chequear menús con más precisión

---
+++ com/adventuresboys/plugin/CollectiblesHolder.java
@@ -0,0 +1,11 @@
+package com.adventuresboys.plugin;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class CollectiblesHolder implements InventoryHolder {
+	@Override
+	public Inventory getInventory() {
+		return null;
+	}
+}
